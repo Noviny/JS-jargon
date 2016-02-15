62b460cf9e632448d37a596f983ce97e2c506446
Noviny BC <benjaminconolly@gmail.com>
2016-02-15 20:30:20 +1100

    making twitter work

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ var app = express();
 
 
 var client = new Twitter({
-  consumer_key: ENV['consumer_key'],
-  consumer_secret: ENV['consumer_secret'],
-  access_token_key: ENV['access_token_key'],
-  access_token_secret: ENV['access_token_key']
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_key
 });
 
 
